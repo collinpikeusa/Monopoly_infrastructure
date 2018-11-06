80ddf8343a964a823b2e76cf0b2901fbbf79fc0f
Collin Pike <collinpikeusa@auburn.edu>
2018-11-06 16:07:26 -0600

    add board

---
+++ Assets/Scenes/TOWER CARDS/DraggableTowerCards.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class DraggableTowerCards :  MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+
+    }
+    public void OnDrag(PointerEventData eventData) {
+        Debug.Log("OnDrag");
+        this.transform.position = eventData.position;
+
+    }
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+
+    }
+}
