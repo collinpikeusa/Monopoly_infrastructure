0bfe7d4422542765ec606d2d8f54caf1cb746193
Collin Pike <collinpikeusa@auburn.edu>
2018-11-09 17:46:19 -0600

    stabalize game

---
+++ Monopoly_infrastructure/DR Prototype Take 2/Assets/Risk Cards/draggable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    [SerializeField] private GameObject back_card;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        if(back_card.activeSelf)
+        {
+            back_card.SetActive(false);
+        }
+    }
+
+}
